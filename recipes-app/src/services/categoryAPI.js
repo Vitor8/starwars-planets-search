db2a8ff0433828b6917e5f1804f34938c74c3518
Vitor Hugo Braz Leite <vitor.h.braz.leite@gmail.com>
2021-08-22 21:36:49 -0300

    Delete recipes-app directory

--- recipes-app/src/services/categoryAPI.js
+++
@@ -1,7 +0,0 @@
-const getCategory = async (endPoint) => {
-  const response = await fetch(endPoint);
-  const data = await response.json();
-  return response.ok ? Promise.resolve(data) : Promise.reject(data);
-};
-
-export default getCategory;
--- recipes-app/src/services/getMealById.js
+++
@@ -1,7 +0,0 @@
-const getMealById = async (endPoint) => {
-  const response = await fetch(endPoint);
-  const data = await response.json();
-  return response.ok ? Promise.resolve(data) : Promise.reject(data);
-};
-
-export default getMealById;
